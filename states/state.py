41bc2b7022d978376000af8f29ec741083a90670
Crux Cook <cruxcook@tutanota.com>
2023-12-19 15:18:26 -0800

    feat(menu): style Menu state
    Menu state is considered as Home state.
    
    Menu state can navigate to About and Pathfinding state.
    
    Issue #1

--- states/state.py
+++ states/state.py
@@ -1,4 +1,6 @@
 import pygame, sys, os
+from pygame.locals import *
+from os import path
 from settings import * 
 
 class State():
@@ -15,6 +17,12 @@ class State():
         pass
     def get_event(self,event):
         pass
+
+    def load_dirs(self):
+        self.current_dir = path.dirname(__file__)
+        self.parent_dir = path.abspath(path.join(self.current_dir, os.pardir))
+        self.assets_dir = path.join(self.parent_dir, "assets")
+        self.icons_dir = path.join(self.assets_dir, 'icons')
     
 def draw_text(text, surface, size, color, x, y):
     font = pygame.font.Font(FONT_NAME, size)
@@ -31,7 +39,16 @@ def draw_rect(x,y,w,h, surface, color):
     pygame.draw.rect(surface, color, rectangle)
     return rectangle
     
-def draw_txt_rect(x,y,w,h, surface, rectColor, text, txt_color, size):
-    rect = draw_rect(x,y,w,h, surface, rectColor)
+def draw_txt_rect(x,y,w,h, surface, rect_color, text, txt_color, size):
+    rect = draw_rect(x,y,w,h, surface, rect_color)
     draw_text(text, surface, size, txt_color, x, y)
     return rect
+
+def draw_grid(surface):
+    for x in range(0, WIDTH, TILE_SIZE):
+        pygame.draw.line(surface, Color(GRID_COLOR),(x,0),(x,HEIGHT))
+    for y in range(0, HEIGHT, TILE_SIZE):
+        pygame.draw.line(surface, Color(GRID_COLOR),(0,y),(WIDTH,y))
+    
+    # draw the last line 
+    pygame.draw.line(surface, Color(GRID_COLOR),(0,HEIGHT),(WIDTH,HEIGHT))
